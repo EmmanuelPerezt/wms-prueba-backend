52e03dccc6e42b7f479e228c43aa9812a4de96fe
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-01 16:38:12 -0600

    feat: add morgan for logging; update middleware and package dependencies

--- app.ts
+++ app.ts
@@ -4,21 +4,22 @@ import { Request, Response } from 'express';
 import { productRoutes } from './src/routes/productos.Routes';
 import { bodegasRoutes } from './src/routes/bodegas.Routes';
 import { corsOptions } from './src/utils/cors';
+import morgan from 'morgan';
 const app = express();
 
 
 //middleware
 app.use(express.json());
-app.use(cors(corsOptions))
-
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
 
 //routes
 app.get('/',(req:Request,res:Response)=>{
     res.send('api corriendo')
-})
+});
 app.use('/api/bodegas',bodegasRoutes);
 app.use('/api/',productRoutes)
 
 app.listen(3000, ()=>{
     console.log('server corriendo en puerto 3000')
-})
+});
