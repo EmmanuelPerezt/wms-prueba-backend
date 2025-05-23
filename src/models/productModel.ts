212a103f4623c2052ea654a8a16a2d967d1bac42
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-23 10:04:22 -0600

    refactor: reorganize product and warehouse model schemas for clarity and consistency

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -3,13 +3,17 @@ import mongoose from "mongoose";
 
 
 const Product = mongoose.model('product', new mongoose.Schema({
-    name: {type: String, required: true},
     SKU: {type: String, required: true},
+    name: {type: String, required: true},
     stock: {type: Number, required: true},
-    package_number: {type: Number, required: true},
-    provider: {type: String, required : true},
-    registration_date: {type: Date, default: Date.now},
-    warehouse_location: {type: mongoose.Schema.Types.ObjectId, ref: 'warehouse', required: true}
+    minimum_stock: {type: Number, required: true},
+    maximum_stock: {type: Number, required: true},
+    lot: {type: String, required: true},
+    serial_number: {type: String, required: true},
+    expiration_date: {type: Date, required: true},
+    warehouse_location: {type: mongoose.Schema.Types.ObjectId, ref: 'warehouse', required: true},
+    pediment: {type: String, required: true},
+    action: {type: String, required: true }
 }));
 
 
--- src/models/warehouseModel.ts
+++ src/models/warehouseModel.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 
 
 const warehouse = mongoose.model('warehouse', new mongoose.Schema({
     name: {type: String, required: true},
-    location: {type: String, required: true},
-    description: {type: String, required: false},
+    hallway_number: {type: Number,required: true},
+    rack_per_hallway: {type: Number,required: true},
+    levels_per_rack: {type: Number,required: true},
 }));
 export {warehouse}
