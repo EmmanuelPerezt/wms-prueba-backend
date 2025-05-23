95cd3f9b958a5e4b75a90e552b4846f40ad8197d
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-23 12:40:51 -0600

    fix: add missing is_Active field to inventory model schema

--- src/models/inventoryModel.ts
+++ src/models/inventoryModel.ts
@@ -3,5 +3,6 @@ import mongoose from "mongoose";
 const Inventory = mongoose.model('inventory', new mongoose.Schema({
     warehouse_location: {type : mongoose.Schema.Types.ObjectId, ref: 'warehouse', required: true},
     products: [{type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true}],
+    is_Active: {type: Boolean, default: true}
 }));
 export {Inventory}
