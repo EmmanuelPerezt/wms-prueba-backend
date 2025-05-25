d059f4b5a3f552465d007353ee0f16ddf930a231
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-24 20:39:08 -0600

    restore: revert revert to bring back model changes

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 
 
 const Product = mongoose.model('product', new mongoose.Schema({
-
-
   name: { type: String, required: true },
   description: { type: String },
   category: { type: String },
