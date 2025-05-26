001d74bbdc63fb939ce39a641ee702ff0ad6b814
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-26 09:01:47 -0600

    fix: unnesesary model

--- src/models/inventoryModel.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const Inventory = mongoose.model('inventory', new mongoose.Schema({
-    warehouse_location: {type : mongoose.Schema.Types.ObjectId, ref: 'warehouse', required: true},
-    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true}],
-    is_Active: {type: Boolean, default: true}
-}));
-export {Inventory}
