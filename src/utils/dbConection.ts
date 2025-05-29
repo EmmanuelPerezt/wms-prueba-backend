47c18afeeb366b052707def4e628b86b23a47e7b
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-05-28 19:07:44 -0600

    fix: update MongoDB connection URI environment variable

--- src/utils/dbConection.ts
+++ src/utils/dbConection.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     try {
-        mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test')
+        mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/test')
 
 
     } catch (error) {
