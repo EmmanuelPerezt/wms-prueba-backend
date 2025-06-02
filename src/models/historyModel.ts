7c9ddd92f482920068f0c87f0209b7281bfa01ad
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-06-02 17:28:35 -0600

    feat: refactor history model

--- src/models/historyModel.ts
+++ src/models/historyModel.ts
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 
 const History = mongoose.model('History', new mongoose.Schema({
-  name: { type: String, required: true },
+  sku: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  id_warehouse: { type: mongoose.Schema.Types.ObjectId, ref: 'Warehouse', required: true },
+  movement_type: { type: String, enum: ['Entrada', 'Salida'], required: true },
+  quantity: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  //pedido pero aun no esta definido
 }));
-
-
 
 
 export { History };
