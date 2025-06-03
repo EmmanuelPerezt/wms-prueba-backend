c8c993feaaa665a49e08a1e4c2fe29c82daf1bab
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-06-03 09:11:43 -0600

    add: comentario

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
 import { readFileSync } from 'fs';
 import { productRoutes } from './src/routes/products.Routes';
-import {warehouseRoutes } from './src/routes/warehouse.Routes';
+import { warehouseRoutes } from './src/routes/warehouse.Routes';
 import morgan from 'morgan';
 import { authRouter } from './src/routes/auth.Routes';
 import { stockRoutes } from './src/routes/stock.Routes';
@@ -18,15 +18,15 @@ app.use(morgan('dev'));
 
 //swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagg));
-
-app.use('/api/warehouse',warehouseRoutes);
-app.use('/api/product',productRoutes)
+//normal routes
+app.use('/api/warehouse', warehouseRoutes);
+app.use('/api/product', productRoutes)
 app.use('/api/stock', stockRoutes);
 
 //auth
-app.use('/api/auth/',authRouter);
+app.use('/api/auth/', authRouter);
 
-app.listen(3000, ()=>{
-    console.log('server corriendo en puerto 3000')
-    console.log('http://localhost:3000/api-docs');
+app.listen(3000, () => {
+  console.log('server corriendo en puerto 3000')
+  console.log('http://localhost:3000/api-docs');
 });
