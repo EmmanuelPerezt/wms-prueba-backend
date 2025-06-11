931fd5ac255c793e776d9dee0b54b4c87f16b5ca
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-06-10 18:04:30 -0600

    feat: refactor UserModel and User Controller fix: create user doesnt work

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,79 +1,86 @@
 import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
 import { User } from '../models/userModel';
+import { connectDB } from '../utils/dbConection';
 import jwt from 'jsonwebtoken';
 
 const registerUser = async (req: Request, res: Response) => {
-    const {name, username, email, password, role } = req.body;
-    if (!name || !username || !email || !password || !role) {
-         res.status(400).json({
-            message: 'Todos los campos son obligatorios'
-        });
-        return;
-    }
-    try {
-        const user = new User({
-            name,
-            username,
-            email,
-            password: bcrypt.hashSync(password, 10),
-            role
-        })
-        await user.save();
-        res.status(201).json({
-            message: 'Usuario registrado'
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error al registrar el usuario',
-            error: error
-        })
-    }
+  const { name, username, email, password, role } = req.body;
+  if (!name || !username || !email || !password || !role) {
+    res.status(400).json({
+      message: 'Todos los campos son obligatorios'
+    });
+    return;
+  }
+  try {
+    await connectDB();
+    const user = new User({
+      name,
+      username,
+      email,
+      password: bcrypt.hashSync(password, 10),
+      role
+    })
+    await user.save();
+    res.status(201).json({
+      message: 'Usuario registrado'
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al registrar el usuario',
+      error
+    })
+    console.error('Error al registrar el usuario:', error);
+  }
 }
-
+const deletUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  // await db.connect();
+}
 const loginUser = async (req: Request, res: Response) => {
-    const { email, password} = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-             res.status(404).json({
-                message: 'Usuario no encontrado'
-            });
-            return;
-        }
-        const isMatch = bcrypt.compareSync(password, user.password);
-        if (!isMatch) {
-             res.status(401).json({
-                message: 'Contraseña incorrecta'
-            });
-            return;
-        }
-        // firmar token
-        const token = jwt.sign(
-            { name: user.name, 
-              email: user.email,
-              role: user.role
-            },
-            process.env.JWT_SECRET || 'secret',
-            { expiresIn: '24h' }
-        )
-        res.status(200).json({
-            message: 'Inicio de sesión exitoso',
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-                token: token
-            }
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error al iniciar sesión',
-            error: error
-        })
-        
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).json({
+        message: 'Usuario no encontrado'
+      });
+      return;
     }
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({
+        message: 'Contraseña incorrecta'
+      });
+      return;
+    }
+    // firmar token
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+        role: user.role
+      },
+      process.env.JWT_SECRET || 'secret',
+      { expiresIn: '24h' }
+    )
+    res.status(200).json({
+      message: 'Inicio de sesión exitoso',
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: token
+      }
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al iniciar sesión',
+      error: error
+    })
+
+  }
 }
 
-export { registerUser, loginUser};
+export { registerUser, loginUser };
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 
 
 const User = mongoose.model('user', new mongoose.Schema({
-    name: {type: String, required: true},
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    dateCreated: {type: Date, default: Date.now},
-    modCreated: {type: Date, default: Date.now},
-    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
-    modifiedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
-    isActive: {type: Boolean, default: true},
-    role: {type: mongoose.Schema.Types.ObjectId, ref: 'role', required: true},
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  dateCreated: { type: Date, default: Date.now },
+  modCreated: { type: Date, default: Date.now },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  modifiedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  isActive: { type: Boolean, default: true },
+  //role: { type: mongoose.Schema.Types.ObjectId, ref: 'role' },
+  role: { type: String, enum: ['admin', 'user'], default: 'user' }
 }));
-export {User}
+export { User }
